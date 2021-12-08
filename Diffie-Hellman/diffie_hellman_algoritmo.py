4c02184192ae54f1912d8301998b8d13d05a24ca
sainzunai <33113726+sainzunai@users.noreply.github.com>
2021-12-08 19:23:28 +0100

    comentarios y orden para facilitar la lectura del codigo

--- Diffie-Hellman/diffie_hellman_algoritmo.py
+++ Diffie-Hellman/diffie_hellman_algoritmo.py
@@ -5,13 +5,13 @@ from random import randint
 P = 23
 G = 9   #base del exponente
 
-print('Valor de P: %d'%P)
-print('Valor de G: %d'%G)
+print("Se establecen los valores P y G para ambos usuarios. \nValor de P: %d; Valor de G: %d\n"%(P, G))
+
 
 
 #Javi escoge la clave privada j
 j = 4
-print('Clave privada de Javi: %d'%j)
+print('Clave privada de Javi j: %d'%j)
 
 #Y genera la nueva clave que posteriormente comparte con Unai
 J = int(pow(G, j, P))
@@ -19,10 +19,13 @@ J = int(pow(G, j, P))
 
 #Unai escoge la clave privada u y genera su clave que posteriormente comparte con Javi
 u = 8
-print('Clave privada de Unai: %d'%u)
+print('Clave privada de Unai u: %d'%u)
 
 U = int(pow(G, u, P))
 
+#Intercambio de claves:
+print("JAVI: Hola Unai mi clave es J: %d" %J)
+print("UNAI: Hola Javi, mi clave es U: %d" %U)
 
 #Una vez intercambiadas las claves, se determina la calve privada compartida
 # Clave privada para Javi
@@ -31,7 +34,5 @@ key_javi = int(pow(U,j,P))
 #Clave privada para Unai
 key_unai = int(pow(J,u,P))
 
-print('Clave Javi: %d'%key_javi)
-print('Clave Unai: %d'%key_unai)
-
-
+print('Clave Javi key_javi: %d'%key_javi)
+print('Clave Unai key_unai: %d'%key_unai)
